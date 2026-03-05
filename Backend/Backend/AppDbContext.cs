6c9f107a46320b95816a28b171ce97674c971fe5
Mehmet <mehmetuygun1925@gmail.com>
2026-03-05 13:33:03 +0300

    UserController oluşturuldu. Login ve Register methodları yazıldı

--- Backend/Backend/AppDbContext.cs
+++ Backend/Backend/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend
 {
@@ -7,5 +8,7 @@ namespace Backend
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        public DbSet<User> Users { get; set; }
+
     }
 }
