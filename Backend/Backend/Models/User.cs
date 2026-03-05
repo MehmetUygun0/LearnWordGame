3c852240119eb61a51dcd5d29813fce27e1f7a36
MoonBorn0 <aaaydogdu@hotmail.com>
2026-03-05 13:18:39 +0300

    user modeli oluşturuldu

---
+++ Backend/Backend/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public required string UserName { get; set; }
+        [Required]
+        public required byte[] Password { get; set; }
+        public DateTime CreatedAt { get; set; } =DateTime.Now;
+    }
+}
