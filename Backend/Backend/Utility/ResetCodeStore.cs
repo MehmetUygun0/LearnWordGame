8363d3400dbff48ebe2c8bfc9a05f8e5deff2984
Mehmet <mehmetuygun1925@gmail.com>
2026-03-30 18:15:32 +0300

    şifre sıfırlama kodların depolanacağı yer

---
+++ Backend/Backend/Utility/ResetCodeStore.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Models;
+
+namespace Backend.Utility
+{
+    public class ResetCodeStore
+    {
+        // Thread-safe bir liste (Aynı anda birden fazla kişi kod isteyebilir)
+        readonly public List<PasswordResetCode> Codes = new List<PasswordResetCode>();
+
+        public void AddCode(int userId, int code)
+        {
+            // Varsa eskisini sil, yenisini ekle
+            //Codes.RemoveAll(x => x.Email == email);
+            Codes.Add(new PasswordResetCode(userId,code) );
+        }
+        public void IsCodeValid(int userId, int code)
+        {
+            Codes.Any(x => x.UserId == userId && x.Code == code);
+        }
+    }
+}
