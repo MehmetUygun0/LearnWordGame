746578d1d082d71c3aa3d4515c654cc4a6680b2e
Mehmet <mehmetuygun1925@gmail.com>
2026-03-30 18:14:21 +0300

    Şifre sıfırlama işlemi için model oluşturuldu

---
+++ Backend/Backend/Models/PasswordResetCode.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Backend.Models
+{
+    public class PasswordResetCode(int UserId, int Code)
+    {
+        public int UserId { get; private set; } = UserId;
+        public int Code { get; private set; } = Code;
+        public DateTime ResetCodeExpiry = DateTime.UtcNow.AddMinutes(3);
+    }
+}
