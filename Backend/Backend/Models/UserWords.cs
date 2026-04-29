872dde263be284ccd352b3f39c49d31c81ebdd02
Mehmet <mehmetuygun1925@gmail.com>
2026-04-29 23:52:42 +0300

    Kullanıcın ekleyeceği kelimeler için model

---
+++ Backend/Backend/Models/UserWords.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class UserWord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; } 
+
+        [Required]
+        [StringLength(100)]
+        public string EngWordName { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string TurWordName { get; set; } = null!;
+
+        public string? Picture { get; set; }
+
+        [MaxLength(20)]
+        public string Level { get; set; } = "UserAdded";
+    }
+}
