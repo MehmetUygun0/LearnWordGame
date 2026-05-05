0b03d91fb4f22429f89a4bdee3a97a4e7b720199
Mehmet <mehmetuygun1925@gmail.com>
2026-05-05 22:05:16 +0300

    Genel bir işlevi yapmak için BaseController oluşturuldu

---
+++ Backend/Backend/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected int? GetUserId()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return int.TryParse(userIdClaim, out int userId)
+                ? userId
+                : null;
+        }
+    }
+}
