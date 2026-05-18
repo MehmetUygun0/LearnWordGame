09682cafbb383474eff52dc9f237cee5df2aa222
Mehmet <mehmetuygun1925@gmail.com>
2026-05-18 18:20:21 +0300

    JwtTOKEN ile ilgili iyileştirme

--- Backend/Backend/Models/DTOs/UserDTOs.cs
+++ Backend/Backend/Models/DTOs/UserDTOs.cs
@@ -25,7 +25,6 @@
     }
     public class RefreshTokenDto
     {
-        public required string AccessToken { get; set; }
         public required string RefreshToken { get; set; }
     }
     public class ProfileLevelStatDto
